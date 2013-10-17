6a10f4f922174ff26572de41773f57df4c5e4b9a
Annie B <asbeug@ancestry.com>
2013-10-16 20:32:13 -0700

    Fixing whitespace

--- Algos/Scheduler/src/edu/calpoly/csc349/Schedule/GreedyScheduler.java
+++ Algos/Scheduler/src/edu/calpoly/csc349/Schedule/GreedyScheduler.java
@@ -5,81 +5,78 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- * This class provides a greedy algorithm implementation to the 
- * Scheduler interface.
+ * This class provides a greedy implementation to the Scheduler interface.
  * @author asbeug@calpoly.edu
  */
 public class GreedyScheduler implements Scheduler {
-	@Override
-	/**
-	 * Create a schedule using a greedy algorithm.
-	 * @param toCover - the time range to cover with employees
-	 * @return employees - the employees availabilities
-	 */
-	public TimeRange[] makeSchedule(TimeRange toCover, TimeRange[] employees) {
-		if (toCover == null) {
-			throw new IllegalArgumentException(
-				"The argument 'toCover' cannot be null.");
-		} else if (employees == null) {
-			throw new IllegalArgumentException(
-				"The argument 'employees' cannot be null.");
-		}
+    @Override
+    /**
+     * Create a schedule using a greedy algorithm.
+     * @param toCover - the time range to cover with employees
+     * @return employees - the employees' availabilities
+     */
+    public TimeRange[] makeSchedule(TimeRange toCover, TimeRange[] employees) {
+        if (toCover == null) {
+            throw new IllegalArgumentException("The argument 'toCover' cannot be null.");
+        } else if (employees == null) {
+            throw new IllegalArgumentException("The argument 'employees' cannot be null.");
+        }
 
-		if (employees.length == 0) {
-			return null;			
-		}
+        if (employees.length == 0) {
+            return null;            
+        }
 
-		ArrayList<TimeRange> scheduleList = new ArrayList<TimeRange>();
-		Arrays.sort(employees, new StartSpanComparator());
+        ArrayList<TimeRange> scheduleList = new ArrayList<TimeRange>();
+        Arrays.sort(employees, new StartSpanComparator());
 
-		int currentEndTime = toCover.getStart();
+        int currentEndTime = toCover.getStart();
 
-		// Evaluate each employee's schedule
-		for (TimeRange currentEmployee: employees) {
-			// Check to see if we're already done
-			if (scheduleList.size() > 0 
-					&& scheduleList.get(0).getStart() <= toCover.getStart() 
-					&& scheduleList.get(scheduleList.size()-1).getEnd() >= toCover.getEnd()) {
-				break;
-			}
-			
-			if (currentEmployee.getStart() <= currentEndTime) {
-				if (currentEmployee.getStart() <= toCover.getStart()  
-						&& scheduleList.size() > 0) {
-					if (currentEmployee.getEnd() >= scheduleList.get(0).getEnd()) {
-						scheduleList.set(0, currentEmployee);
-						currentEndTime = currentEmployee.getEnd();
-					}
-				} else if (currentEmployee.getEnd() > currentEndTime) {
-					scheduleList.add(currentEmployee);
-					currentEndTime = currentEmployee.getEnd();
-				}
-			}
-		}
+        // Evaluate each employee's schedule
+        for (TimeRange currentEmployee : employees) {
+            // Check to see if we're already done
+            if (scheduleList.size() > 0 
+                    && scheduleList.get(0).getStart() <= toCover.getStart() 
+                    && scheduleList.get(scheduleList.size()-1).getEnd() >= toCover.getEnd()) {
+                break;
+            }
+            
+            if (currentEmployee.getStart() <= currentEndTime) {
+                if (currentEmployee.getStart() <= toCover.getStart()  
+                        && scheduleList.size() > 0) {
+                    if (currentEmployee.getEnd() >= scheduleList.get(0).getEnd()) {
+                        scheduleList.set(0, currentEmployee);
+                        currentEndTime = currentEmployee.getEnd();
+                    }
+                } else if (currentEmployee.getEnd() > currentEndTime) {
+                    scheduleList.add(currentEmployee);
+                    currentEndTime = currentEmployee.getEnd();
+                }
+            }
+        }
 
-		if (scheduleList.size() > 0 
-				&& scheduleList.get(scheduleList.size()-1).getEnd() 
-				>= toCover.getEnd()) {
-			return scheduleList.toArray(new TimeRange[] {});
-		} else {
-			return null;
-		}
-	}
+        if (scheduleList.size() > 0 
+                && scheduleList.get(scheduleList.size()-1).getEnd() 
+                >= toCover.getEnd()) {
+            return scheduleList.toArray(new TimeRange[] {});
+        } else {
+            return null;
+        }
+    }
 
-	/**
-	 * Sort first by start time (ascending) then by span (descending).
-	 */
-	public class StartSpanComparator implements Comparator<TimeRange> {
-		@Override
-		public int compare(TimeRange o1, TimeRange o2) {
-			if (o1.getStart() == o2.getStart()) {
-				int span1 = o1.getEnd() - o1.getStart();
-				int span2 = o2.getEnd() - o2.getStart();
-				return Integer.valueOf(span2).compareTo(Integer.valueOf(span1));
-			}
+    /**
+     * Sort first by start time (ascending) then by span (descending).
+     */
+    public class StartSpanComparator implements Comparator<TimeRange> {
+        @Override
+        public int compare(TimeRange o1, TimeRange o2) {
+            if (o1.getStart() == o2.getStart()) {
+                int span1 = o1.getEnd() - o1.getStart();
+                int span2 = o2.getEnd() - o2.getStart();
+                return Integer.valueOf(span2).compareTo(Integer.valueOf(span1));
+            }
 
-			return Integer.valueOf(o1.getStart()).compareTo(
-					Integer.valueOf(o2.getStart()));
-		}
-	}
+            return Integer.valueOf(o1.getStart()).compareTo(
+                    Integer.valueOf(o2.getStart()));
+        }
+    }
 }
--- Networks/Sliding/debugger.h
+++ Networks/Sliding/debugger.h
@@ -1,47 +1,54 @@
 /*********************************************
  * File         : debugger.h                 *
  * Author       : Annie Beug                 *
- * Date			: 28-FEB-2010                *
+ * Date         : 28-FEB-2010                *
  * Modified     : 08-MAR-2010                *
+ * ------------------------------------------*
+ * This file contains macros for printing    *
+ * debug statements for the set debug level. *
+ * Set _DEBUG_LEVEL to the desired level for *
+ * regular debug output and _DEBUG_ERRORS to *
+ * 0 or 1 to turn error debug printing off   *
+ * or on.
  *********************************************/
 
 #include <stdio.h>
 
-#define	DEBUG_PRINT		0
-#define DEBUG_MIN		1
-#define DEBUG_MEDIUM	2
-#define DEBUG_MAX		3
+#define DEBUG_PRINT     0
+#define DEBUG_MIN       1
+#define DEBUG_MEDIUM    2
+#define DEBUG_MAX       3
 
-#define _DEBUG_LEVEL	DEBUG_MAX
-#define _DEBUG_ERRORS	1
+#define _DEBUG_LEVEL    DEBUG_MAX
+#define _DEBUG_ERRORS   1
 
 // Print no matter what
 #if _DEBUG_LEVEL >= DEBUG_PRINT
-	#define debug_print printf
+    #define debug_print printf
 # else
-	#define debut_print
+    #define debut_print
 #endif
 // Print minimal amount
 #if _DEBUG_LEVEL >= DEBUG_MIN
-	#define debug_min printf
+    #define debug_min printf
 # else
-	#define debug_min
+    #define debug_min
 #endif
 // Print most things
 #if _DEBUG_LEVEL >= DEBUG_MEDIUM
-	#define debug_medium printf
+    #define debug_medium printf
 #else
-	#define debug_medium
+    #define debug_medium
 #endif
 // Print everything
 #if _DEBUG_LEVEL >= DEBUG_MAX
-	#define debug_max printf
+    #define debug_max printf
 # else
-	#define debug_max
+    #define debug_max
 #endif
 
 #ifdef _DEBUG_ERRORS
-	#define debug_errors printf
+    #define debug_errors printf
 #else
-	#define debug_errors
+    #define debug_errors
 #endif
